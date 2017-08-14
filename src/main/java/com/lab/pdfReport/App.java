c587f4a802499d31da0a348bb76558647cbb07eb
RobberYao <ychh1015@sina.cn>
2017-08-14 18:43:02 +0800

    Create PdfReport

---
+++ src/main/java/com/lab/pdfReport/App.java
@@ -0,0 +1,13 @@
+package com.lab.pdfReport;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
